319122ca4a67e25b2acb9bd20a27f5bc4414c66d
RONIASHARI <roniashari1234@gmail.com>
2024-06-05 10:21:17 +0700

    commit login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Belajar Controller & View pada Laravel</title>
-    @vite('resources/sass/app.scss')
+    {{-- @vite('resources/sass/app.scss') --}}
 </head>
 <body style="background-color: bisque">
     <div class="container text-center my-5">
@@ -40,6 +40,6 @@
             </div>
         </div>
     </div>
-    @vite('resources/js/app.js')
+    {{-- @vite('resources/js/app.js') --}}
 </body>
 </html>
